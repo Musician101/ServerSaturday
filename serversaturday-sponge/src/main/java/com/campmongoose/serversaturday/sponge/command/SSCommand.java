b337bf3e8ebd253142f6548d69b840fb10d76172
Bruce <bruceabivens@gmail.com>
2017-04-09 12:03:29 -0700

    Fix built in help command.

--- serversaturday-sponge/src/main/java/com/campmongoose/serversaturday/sponge/command/SSCommand.java
+++ serversaturday-sponge/src/main/java/com/campmongoose/serversaturday/sponge/command/SSCommand.java
@@ -4,8 +4,8 @@ import com.campmongoose.serversaturday.common.Reference;
 import com.campmongoose.serversaturday.sponge.SpongeServerSaturday;
 import javax.annotation.Nonnull;
 import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.CommandCallable;
 import org.spongepowered.api.command.CommandException;
-import org.spongepowered.api.command.CommandMapping;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
@@ -17,10 +17,12 @@ public class SSCommand extends AbstractSpongeCommand {
     @Nonnull
     @Override
     public CommandResult execute(@Nonnull CommandSource source, @Nonnull CommandContext args) throws CommandException {
-        Text.join(Text.builder("===== ").color(TextColors.GREEN).build(), Text.of(Reference.NAME + " v" + Reference.VERSION), Text.builder(" by ").color(TextColors.GREEN).build(), Text.of("Musician101"), Text.builder(" =====").color(TextColors.GREEN).build());
-        Sponge.getCommandManager().getOwnedBy(SpongeServerSaturday.instance()).stream().map(CommandMapping::getCallable)
-                .filter(command -> command.testPermission(source))
-                .forEach(command -> command.getHelp(source));
+        source.sendMessage(Text.join(Text.builder("===== ").color(TextColors.GREEN).build(), Text.of(Reference.NAME + " v" + Reference.VERSION), Text.builder(" by ").color(TextColors.GREEN).build(), Text.of("Musician101"), Text.builder(" =====").color(TextColors.GREEN).build()));
+        Sponge.getCommandManager().getOwnedBy(SpongeServerSaturday.instance()).forEach(commandMapping -> {
+            CommandCallable command = commandMapping.getCallable();
+            command.getShortDescription(source).ifPresent(description ->
+                    source.sendMessage(Text.join(Text.builder("/" + commandMapping.getPrimaryAlias() + " ").color(TextColors.GRAY).build(), command.getUsage(source), Text.of(" "), Text.builder().append(description).color(TextColors.AQUA).build())));
+        });
 
         return CommandResult.success();
     }
