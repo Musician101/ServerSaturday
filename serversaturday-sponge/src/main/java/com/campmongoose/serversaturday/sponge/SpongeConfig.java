3e2a9a8e18a905ef8decd302130d464e27b12231
Bruce <bruceabivens@gmail.com>
2017-04-09 11:49:03 -0700

    Attempt to fix errors in SpongeConfig.

--- serversaturday-sponge/src/main/java/com/campmongoose/serversaturday/sponge/SpongeConfig.java
+++ serversaturday-sponge/src/main/java/com/campmongoose/serversaturday/sponge/SpongeConfig.java
@@ -3,26 +3,27 @@ package com.campmongoose.serversaturday.sponge;
 import com.campmongoose.serversaturday.common.AbstractConfig;
 import com.campmongoose.serversaturday.common.Reference.Config;
 import com.campmongoose.serversaturday.common.Reference.Messages;
-import java.io.File;
 import java.io.IOException;
 import javax.annotation.Nonnull;
 import ninja.leaping.configurate.ConfigurationNode;
 import ninja.leaping.configurate.commented.CommentedConfigurationNode;
 import ninja.leaping.configurate.loader.ConfigurationLoader;
 import org.slf4j.Logger;
+import org.spongepowered.api.config.ConfigRoot;
 
 public class SpongeConfig extends AbstractConfig {
 
-    private ConfigurationNode config;
-    private final ConfigurationLoader<CommentedConfigurationNode> configManager;
+    private ConfigurationLoader<CommentedConfigurationNode> configManager;
 
-    public SpongeConfig(@Nonnull ConfigurationLoader<CommentedConfigurationNode> configManager, @Nonnull File configFile) {
-        super(configFile);
-        this.configManager = configManager;
+    public SpongeConfig(@Nonnull ConfigRoot configRoot) {
+        super(configRoot.getConfigPath().toFile());
+        this.configManager = configRoot.getConfig();
+        reload();
     }
 
     @Override
     public void reload() {
+        ConfigurationNode config;
         SpongeServerSaturday plugin = SpongeServerSaturday.instance();
         Logger logger = plugin.getLogger();
         try {
@@ -36,10 +37,12 @@ public class SpongeConfig extends AbstractConfig {
                 logger.info("Default file created.");
             }
 
+            //TODO still throwing NPE
             config = configManager.load();
         }
         catch (IOException e) {
             logger.info(Messages.ioException(configFile));
+            return;
         }
 
         //Config update code goes here when ready
