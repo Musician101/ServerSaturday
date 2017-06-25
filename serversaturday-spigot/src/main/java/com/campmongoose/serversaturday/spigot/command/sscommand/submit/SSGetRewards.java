ed3a5cd60f4384d01fdaf5290b1cdb2a5a844d85
Bruce <bruceabivens@gmail.com>
2017-06-25 00:40:00 -0700

    Add missing usage and permissions assignments.

--- serversaturday-spigot/src/main/java/com/campmongoose/serversaturday/spigot/command/sscommand/submit/SSGetRewards.java
+++ serversaturday-spigot/src/main/java/com/campmongoose/serversaturday/spigot/command/sscommand/submit/SSGetRewards.java
@@ -2,7 +2,12 @@ package com.campmongoose.serversaturday.spigot.command.sscommand.submit;
 
 import com.campmongoose.serversaturday.common.Reference.Commands;
 import com.campmongoose.serversaturday.common.Reference.Messages;
+import com.campmongoose.serversaturday.common.Reference.Permissions;
 import com.campmongoose.serversaturday.spigot.command.AbstractSpigotCommand;
+import com.campmongoose.serversaturday.spigot.command.SpigotCommandArgument;
+import com.campmongoose.serversaturday.spigot.command.SpigotCommandPermissions;
+import com.campmongoose.serversaturday.spigot.command.SpigotCommandUsage;
+import java.util.Collections;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
@@ -10,6 +15,8 @@ public class SSGetRewards extends AbstractSpigotCommand {
 
     public SSGetRewards() {
         super(Commands.GET_REWARDS_NAME, Commands.GET_REWARDS_DESC);
+        usage = new SpigotCommandUsage(Collections.singletonList(new SpigotCommandArgument(Commands.SS_CMD + Commands.GOTO_NAME)));
+        permissions = new SpigotCommandPermissions(Permissions.VIEW_GOTO, false);
         executor = (sender, args) -> {
             getPluginInstance().getRewardGiver().givePlayerReward((Player) sender);
             sender.sendMessage(ChatColor.GOLD + Messages.REWARDS_GIVEN);
