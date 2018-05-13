4d80fa9546507482e48d1593a32e48eb3fab3bf3
Bruce (Musician101) <bruceabivens@gmail.com>
2018-05-13 08:24:22 -0700

    VanillaGradle is being dumb and breaking in this setup so trying to split it.

--- serversaturday-common/src/main/java/com/campmongoose/serversaturday/common/TriConsumer.java
+++
@@ -1,9 +0,0 @@
-package com.campmongoose.serversaturday.common;
-
-import javax.annotation.Nullable;
-
-@FunctionalInterface
-public interface TriConsumer<T, U, V> {
-
-    void accept(@Nullable T t, @Nullable U u, @Nullable V v);
-}
