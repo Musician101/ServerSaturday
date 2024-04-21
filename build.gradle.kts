e48fa21b43b82654594f18bb98fdd445710e6f73
Musician101 <bruceabivens@gmail.com>
2024-04-21 15:48:37 -0700

    Update to 1.20.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 }
 
 dependencies {
-    compileOnlyApi("io.papermc.paper:paper-api:1.20.2-R0.1-SNAPSHOT")
+    compileOnlyApi("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
     api("com.github.Musician101:Bukkitier:1.3.3") {
         exclude("org.spigotmc")
     }
