8e4b2abf5532c74981565c0e633488f292629bcc
Musician101 <bruceabivens@gmail.com>
2024-05-08 19:57:27 -0700

    More plugins to make things easier.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import xyz.jpenilla.resourcefactory.bukkit.Permission
+
 buildscript {
     configurations {
         classpath {
@@ -10,9 +12,11 @@ buildscript {
 }
 
 plugins {
-    java
     `java-library`
-    id("com.github.johnrengelman.shadow")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("io.papermc.paperweight.userdev") version "1.7.0"
+    id("xyz.jpenilla.run-paper") version "2.2.4"
+    id("xyz.jpenilla.resource-factory-bukkit-convention") version "1.1.1"
 }
 
 group = "com.campmongoose"
@@ -26,7 +30,7 @@ repositories {
 }
 
 dependencies {
-    compileOnlyApi("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+    paperweight.paperDevBundle("1.20.4-R0.1-SNAPSHOT")
     api("com.github.Musician101:Bukkitier:2.0.0")
     api("com.github.musician101.musigui:paper:1.2.2") {
         exclude("io.papermc.paper")
@@ -62,4 +66,45 @@ tasks {
         from("build/libs/${project.name}-${project.version}.jar")
         into("server/plugins")
     }
+}
+
+bukkitPluginYaml {
+    main = "com.campmongoose.serversaturday.ServerSaturday"
+    author = "Musician101"
+    apiVersion = "1.20"
+    commands.create("serversaturday") {
+        aliases.addAll("ss")
+        description = "Displays help and plugin info."
+        usage = "/serversaturday"
+    }
+    permissions {
+        create("ss.*") {
+            default = Permission.Default.OP
+            description = "Gives access to all features."
+            children("ss.admin", "ss.feature", "ss.reload", "ss.submit", "ss.view", "ss.view.goto")
+        }
+        create("ss.admin") {
+            default = Permission.Default.OP
+            description = "Gives access to admin features."
+        }
+        create("ss.feature") {
+            default = Permission.Default.OP
+            description = "Gives access to feature related features."
+        }
+        create("ss.submit") {
+            default = Permission.Default.TRUE
+            description = "Gives access to the submit features."
+        }
+        create("ss.view") {
+            default = Permission.Default.TRUE
+            description = "Gives access to basic view features."
+        }
+        create("ss.view.goto") {
+            default = Permission.Default.TRUE
+            description = "Gives access to teleporting to build features."
+            children("ss.view")
+        }
+    }
+
+    //foliaSupported = true;
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,1 @@
 rootProject.name = "ServerSaturday"
-pluginManagement {
-    plugins {
-        `java-library`
-        java
-        id("com.github.johnrengelman.shadow") version "8.1.1"
-    }
-}
