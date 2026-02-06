2fc4cfcd3e347f4fe29437019f4a50a2bf62ceef
Musician101 <bruceabivens@gmail.com>
2026-02-06 15:33:28 -0800

    Fix SubmitterArgumenType.

--- src/main/java/com/campmongoose/serversaturday/command/argument/SubmitterArgumentType.java
+++ src/main/java/com/campmongoose/serversaturday/command/argument/SubmitterArgumentType.java
@@ -2,22 +2,22 @@ package com.campmongoose.serversaturday.command.argument;
 
 import com.campmongoose.serversaturday.submission.Submissions;
 import com.campmongoose.serversaturday.submission.Submitter;
-import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import io.papermc.paper.command.brigadier.argument.CustomArgumentType;
 import org.jspecify.annotations.NullMarked;
 
 import java.util.concurrent.CompletableFuture;
 
-import static com.campmongoose.serversaturday.Messages.PREFIX;
 import static com.campmongoose.serversaturday.ServerSaturday.getPlugin;
 
 @NullMarked
-public class SubmitterArgumentType implements ArgumentType<Submitter> {
+public class SubmitterArgumentType implements CustomArgumentType.Converted<Submitter, String> {
 
     private Submissions getSubmissions() {
         return getPlugin().getSubmissions();
@@ -30,7 +30,12 @@ public class SubmitterArgumentType implements ArgumentType<Submitter> {
     }
 
     @Override
-    public Submitter parse(StringReader stringReader) throws CommandSyntaxException {
-        return getSubmissions().getSubmitter(stringReader.readString()).orElseThrow(() -> new SimpleCommandExceptionType(() -> PREFIX + "Could not find a player with that name.").create());
+    public ArgumentType<String> getNativeType() {
+        return StringArgumentType.word();
+    }
+
+    @Override
+    public Submitter convert(String nativeType) throws CommandSyntaxException {
+        return getSubmissions().getSubmitter(nativeType).orElseThrow(() -> new SimpleCommandExceptionType(() -> "Could not find a player with that name.").create());
     }
 }
